235cf1c3ac328d383e3ac51c2e091e532a437088
Bagus <bagusdp.1811010081@mail.darmajaya.ac.id>
2025-01-05 19:43:06 +0700

    first commit

---
+++ BackendHotel/src/test/java/com/backendHotel/BackendHotel/BackendHotelApplicationTests.java
@@ -0,0 +1,13 @@
+package com.backendHotel.BackendHotel;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendHotelApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
